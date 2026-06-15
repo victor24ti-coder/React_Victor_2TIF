1aedf5d188fefca1efc3407d011e0ad0cb22161e
unknown <victor24ti@mahasiswa.pcr.ac.id>
2026-06-15 08:21:25 +0700

    Update Customers page, tailwind, dan dependencies

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,47 @@
-import PageHeader from "../components/pageHeader"
-export default function Customers(){
-    return(
-        <div>
-            <PageHeader/>
-            <h1>Ini halaman Customers</h1>
+import PageHeader from "../components/pageHeader";
+export default function Customers() {
+  return (
+    <div>
+      <PageHeader />
+      <h1>Ini halaman Customers</h1>
+      <button
+        className="btn"
+        onClick={() => document.getElementById("my_modal_1").showModal()}
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
         </div>
-    )
+      </dialog>
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
